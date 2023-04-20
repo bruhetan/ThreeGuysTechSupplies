a53708e50cb67f50576e2bd75703857ba1b87014
BeaVert16 <afapenha@gmail.com>
2023-04-20 05:24:50 -0400

    simplified js code

--- scripts/checkout/checkout.js
+++ scripts/checkout/checkout.js
@@ -1,18 +1,3 @@
-// function checkTopicSelection() {
-//     var selectedOption = document.getElementsByTagName("select")[0].value;
-//     var delivery = document.getElementById('Delivery');
-//     var pickup = document.getElementById('Store-Pickup');
-
-//     delivery.style.display = "none";
-//     pickup.style.display = "none";
-
-//     if (selectedOption === "1") {
-//         delivery.style.display = "block";
-//     } else if (selectedOption === "2") {
-//         pickup.style.display = "block";
-//     }
-// }
-
 function submitForm() {
     alert("Thank you for contacting us! We will get back to you as soon as possible.");
 }
